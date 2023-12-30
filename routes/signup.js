7686a9a76aa8b09f36c3272ed00287c6e7ae6bb8
NithishKumarstack <officialnithish13@gmail.com>
2023-12-30 18:54:58 +0530

    Server

--- routes/signup.js
+++ routes/signup.js
@@ -8,12 +8,10 @@ const bcrypt = require("bcrypt");
 router.post("/", async (req, res) => {
 	try {
 		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+		if (error) return res.status(400).send({ message: error.details[0].message });
 
 		let user = await User.findOne({ email: req.body.email });
-		if (user)
-			return res.status(409).send({ message: "User with given email already Exist!" });
+		if (user) return res.status(409).send({ message: "User with given email already Exist!" });
 
 		const salt = await bcrypt.genSalt(Number(process.env.SALT));
 		const hashPassword = await bcrypt.hash(req.body.password, salt);
